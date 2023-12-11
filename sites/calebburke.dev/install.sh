1d41ba87de9325d732e831d5702f3cec543edf6a
caleb <calebburke91@gmail.com>
2023-12-11 00:04:04 -0800

    new

---
+++ sites/calebburke.dev/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# @file sites/calebburke.dev/install.sh
+# @author Caleb Burke
+# @date Dec 11, 2023
+
+yes | apt install node npm
+
--- sites/calebburke.dev/start.sh
+++ sites/calebburke.dev/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# @file sites/calebburke.dev
+# @file sites/calebburke.dev/start.sh
 # @author Caleb Burke
 # @date Dec 10, 2023
 
+./install.sh
 npm run --prefix /var/sites/calebburke.dev/www start
 
